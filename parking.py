56d45a3ff2b509de749acaa4308ca9aa6376af06
kusuma-21 <nkusuma2992@gmail.com>
2022-12-07 13:11:23 +0530

    first commit

---
+++ parking.py
@@ -0,0 +1,49 @@
+#if else
+# n=10
+# if(n<=0):
+#     print('less than zero')
+# elif(n==10):
+#      print('value is lessthan equal to five')
+# else:
+#     print('n is higher tham ten')
+# students = ['rahul','ram','rajesh']
+# print('the students name are:')
+# for s in students:
+#     print(s)
+#7
+#listsss......................................
+# l = [1,2,3,5,4,5, 'aditya',True]
+# # print(l)
+# print(l[0])
+# print(l[4])
+# print(l[1:5])
+# print(l[2:1])
+# print(l[3:7])
+# print(l*2)
+l1=['ammu','kusuma']
+t = [1,2,3,5,4,5, 'aditya',True]
+details = { 'ammu': 93, 'pallu' : 66, 'pooja' : 56, 'roja' : 22}
+details[4]='test'
+# print(details)
+# print(details.values()) #inbuilt function for printing numbers
+# print(details.keys()) #inbilt functions for printing names
+# print(details[4])
+# print(t[0])
+# print(t[1:4])
+# print(t[-3:])
+# print(l1+l)
+# m=int(input())
+# l=list(map(int,input().split()))
+# vasvi
+# for info in dict:
+#     print(info)
+# for info1 in dict:
+#     print(dict(info1))
+# college = {}
+name = input("enter college name :")
+rollnum = input('enter Rollno:')
+# college[name]=[rollnum]
+# print(college)
+college1 = {name:rollnum}
+print(college1)
+#list tuples users taking input dictionary, tuples first bracket, dictonary curly brackets
